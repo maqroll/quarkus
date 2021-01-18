63ad8a9fbfda073dcfbc805df5c23b8d5ad10d22
maqroll <loteroc@gmail.com>
2021-01-18 08:57:15 +0100

    ! logging

--- src/main/java/org/my/group/WebSocket.java
+++ src/main/java/org/my/group/WebSocket.java
@@ -17,10 +17,12 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.util.ArrayList;
 import java.util.List;
+import org.jboss.logging.Logger;
 
 @ServerEndpoint("/wss/{id}")
 @ApplicationScoped
 public class WebSocket {
+  private static final Logger LOG = Logger.getLogger(WebSocket.class);
 
   // TODO: synchronization???
   private List<Session> sessions = new ArrayList<>();
@@ -33,20 +35,24 @@ public class WebSocket {
   @OnOpen
   public void onOpen(Session session, @PathParam("id") String username) {
     sessions.add(session);
+    LOG.debug("New websocket session");
   }
 
   @OnClose
   public void onClose(Session session, @PathParam("id") String username) {
     sessions.remove(session);
+    LOG.debug("Close websocket session");
   }
 
   @OnError
   public void onError(Session session, @PathParam("id") String username, Throwable throwable) {
     sessions.remove(session);
+    LOG.warn("Error on websocket session", throwable);
   }
 
   @OnMessage
   public void onMessage(String message, @PathParam("id") String username) {
+    // discarded by design
   }
 
   @PostConstruct
@@ -54,7 +60,7 @@ public class WebSocket {
     subscription = prices.subscribe((Double price) -> sessions.forEach(s -> {
       s.getAsyncRemote().sendObject(price, result -> {
         if (result.getException() != null) {
-          System.out.println("Unable to send message: " + result.getException());
+          LOG.warn("Unable to send message", result.getException());
         }
       });
     }));
@@ -69,7 +75,7 @@ public class WebSocket {
     sessions.stream().forEach(s -> {
       s.getAsyncRemote().sendObject(message, result -> {
         if (result.getException() != null) {
-          System.out.println("Unable to send message: " + result.getException());
+          LOG.warn("Unable to send message",result.getException());
         }
       });
     });
